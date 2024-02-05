ad28ff19d66287086d912882750ffed55ba308db
AnandYellapu <anandsaiii1200@gmail.com>
2024-02-05 19:33:48 +0530

    update

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -865,7 +865,7 @@ console.log(reminder);
 
       if (user) {
         const selectedReminderTime = newTask.reminder.getTime();
-        const currentDateTime = Date.now();
+        const currentDateTime = Date.now() + 18000000;
         const initialTimeDifference = selectedReminderTime - currentDateTime;
         console.log('CurrentDateTime',currentDateTime);
         console.log('InitialDateTime',initialTimeDifference);
