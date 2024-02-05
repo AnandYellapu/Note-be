1b8fd354e4fb6eabea5cea75788f0db21af253cc
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2024-02-05 15:58:51 +0530

    Update taskController.js

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -75,9 +75,9 @@ const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: true, // true for 465, false for other ports
+  // host: process.env.SMTP_HOST,
+  // port: process.env.SMTP_PORT,
+  // secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD,
